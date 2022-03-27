a2489b9d6d16847d0e2b63b96baf4ea036a8f557
PanAndrei <andrei_kmv@inbox.ru>
2022-03-27 19:33:26 +0300

    init commit. add extensions

--- TaxiApp/Controller/LoginController.swift
+++ TaxiApp/Controller/LoginController.swift
@@ -27,9 +27,6 @@ class LoginController: UIViewController {
         view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
         
         view.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
